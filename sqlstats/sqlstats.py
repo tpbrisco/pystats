ffb1a1a195ff9c4bc9b10001c75441c5076a05e6
tpbrisco <tpbrisco+gh@gmail.com>
2018-04-24 06:33:27 -0400

    move test variables to command line

--- sqlstats/sqlstats.py
+++ sqlstats/sqlstats.py
@@ -1,20 +1,65 @@
 # a very simple query point to check mysql galera status and return json
 import os, sys
+import getopt
 from flask import Flask, render_template, json, request, jsonify, Response
 from flaskext.mysql import MySQL
 
+# parse command line opts - return dictionary with basic params
+# priority for settings variables:
+#  - command line
+#  - config file
+#  - environment variables
+# -f config.txt
+# and set variables based on environment variables
+def get_cmd_opts():
+    # set variables based on environment variables first
+    stat_dbsvc=os.getenv('STAT_DBSVC')
+    # next are really only used if no database service is discovered
+    stat_dbusr=os.getenv('STAT_DBUSR')
+    stat_dbpwd=os.getenv('STAT_DBPWD')
+    stat_dbdb=os.getenv('STAT_DBDB')
+    stat_dbhost=os.getenv('STAT_DBHOST')
+    # get variables from config file
+    # punt for now
+    # get variables from command line
+    try:
+        options, remainder = getopt.getopt(sys.argv[1:], 's:u:p:d:h:', ['service=',
+                                                                   'username=',
+                                                                   'password=',
+                                                                   'database=',
+                                                                   'host='])
+    except getopt.GetoptError as err:
+        print('ERROR:', err)
+        sys.exit(1)
+    
+    for opt, arg in options:
+        if opt in ('-s', '--service'):
+            stat_dbsvc = arg
+        elif opt in ('-u', '--username'):
+            stat_dbusr = arg
+        elif opt in ('-p', '--password'):
+            stat_dbpwd = arg
+        elif opt in ('-d', '--database'):
+            stat_dbdb = arg
+        elif opt in ('-h', '--host'):
+            stat_dbhost = arg
+
+    return (stat_dbsvc, stat_dbusr, stat_dbpwd, stat_dbdb, stat_dbhost)
+
 app = Flask(__name__)
 mysql = MySQL()
+
+(db_service, db_username, db_password, db_database, db_host) = get_cmd_opts()
 
 # get service instance information, and go
 vcap_svc = os.getenv("VCAP_SERVICES", "")
 print "vcap_svc:", vcap_svc
 if vcap_svc == '':
     print "No service bindings, defaulting to local/debug configuration"
-    app.config['MYSQL_DATABASE_USER'] = 'testuser'
-    app.config['MYSQL_DATABASE_PASSWORD'] = 'test123'
-    app.config['MYSQL_DATABASE_DB'] = ''
-    app.config['MYSQL_DATABASE_HOST'] = 'npm.inmyshorts.org'
+    app.config['MYSQL_DATABASE_USER'] = db_username # 'testuser'
+    app.config['MYSQL_DATABASE_PASSWORD'] = db_password # 'test123'
+    app.config['MYSQL_DATABASE_DB'] = db_database # ''
+    app.config['MYSQL_DATABASE_HOST'] = db_host # 'npm.inmyshorts.org'
 else:
     vcap = json.loads(vcap_svc)
     db_svc = ''
