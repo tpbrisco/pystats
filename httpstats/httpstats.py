378e1748ca355ce61d05f17031882830610c1919
tpbrisco <tpbrisco+gh@gmail.com>
2019-05-26 21:54:10 -0400

    using histograms and gauges

--- httpstats/httpstats.py
+++ httpstats/httpstats.py
@@ -3,10 +3,11 @@ import os, sys
 import time
 import requests
 import logging
-from prometheus_client import start_http_server, Gauge
+from prometheus_client import start_http_server, Histogram, Gauge
 from argparse import ArgumentParser
 
 logging.basicConfig(level=logging.DEBUG)
+
 
 def get_cmd_opts():
     TARGET_URL = os.getenv('TARGET_URL')
@@ -21,7 +22,6 @@ def get_cmd_opts():
     return parser.parse_args()
 
 
-
 def do_transact(url):
     logging.info('url:{}'.format(url))
     start = time.time()
@@ -33,14 +33,18 @@ def do_transact(url):
     end = time.time()
     return end - start
 
+
 args = get_cmd_opts()
-xact_time = Gauge('http_get_time', 'Time to look up URL {}'.format(args.url))
+# xact_time = Gauge('http_get_time', 'Time to look up URL {}'.format(args.url))
+xact_time = Histogram('http_get_time_seconds', 'Time to lookup URL {}'.format(args.url))
+xact_gauge = Gauge('http_get_time_seconds', 'Time to lookup URL {}'.format(args.url))
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", default=8000))
     logging.info("Starting; listening on {} target is {}".format(port, args.url))
-    xact_time.set(do_transact(args.url))
     start_http_server(port)
     while True:
+        t_time = do_transact(args.url)
+        xact_time.observe(t_time)
+        xact_gauge.set(t_time)
         time.sleep(30)
-        xact_time.set(do_transact(args.url))
